ba8d4469a2c88adb2eb544753eabed914723199d
Aryan rathi <choudharyaryanrathi0409@gmail.com>
2026-07-23 16:54:07 +0530

    Implement baud rate generator module

--- rtl/baud_generator.v
+++ rtl/baud_generator.v
@@ -1,1 +1,32 @@
 // Baud Generator
+module baud_gen(
+    input clk,
+    input clr,
+    output reg baud_tick
+);
+
+reg [13:0] count;
+
+always @(posedge clk or negedge clr)
+begin
+    if(!clr)
+    begin
+        count <= 0;
+        baud_tick <= 0;
+    end
+    else
+    begin
+        if(count == 10415)
+        begin
+            count <= 0;
+            baud_tick <= 1;   // pulse for 1 clock
+        end
+        else
+        begin
+            count <= count + 1;
+            baud_tick <= 0;
+        end
+    end
+end
+
+endmodule
